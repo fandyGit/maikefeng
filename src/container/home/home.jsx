6d886c29e28a1b85f9a7ce9b7c3167cadf0215cb
luhuan917 <785470041@qq.com>
2018-10-11 11:14:03 +0800

    电脑办公列表布局，，复用之前的组件

--- src/container/home/home.jsx
+++ src/container/home/home.jsx
@@ -23,6 +23,10 @@ export default class Home extends Component{
     layouth2:[],
     layouth3:[],
     layouti:[],
+    layouti1:[],
+    layouti2:[],
+    layouti3:[],
+
   }
   componentWillMount(){
     this.$http.get('http://www.51mkf.com/mobile/index.php?act=index').then(res=>{
@@ -40,6 +44,9 @@ export default class Home extends Component{
           layouth2:result.datas[14]['home5']['item'],
           layouth3:result.datas[15]['home5']['item'],
           layouti:result.datas[18]['goods']['item'],
+          layouti1:result.datas[21]['goods']['item'],
+          layouti2:result.datas[24]['home3']['item'],
+          layouti3:result.datas[25]['goods']['item'],
         })
       }
     })
@@ -52,7 +59,7 @@ export default class Home extends Component{
   }
   render(){
     const {banners,layoutc,layoutd,layoute,layoutf,layoutg,layouth1,
-      layouth2,layouth3,layouti}=this.state;
+      layouth2,layouth3,layouti,layouti1,layouti2,layouti3}=this.state;
     return (
       <div>
         <div className='wrapper'>
@@ -125,6 +132,21 @@ export default class Home extends Component{
               </a>
               <img style={{display:'block',height:'43px'}} className='layout-f' src="http://www.51mkf.com/data/upload/mobile/special/s0/s0_05506778612994991.png" alt=""/>
               <Layouti layouti={layouti}></Layouti>
+            </li>
+            <li>
+              <img style={{display:'block',height:'43px'}} className='layout-f' src="http://www.51mkf.com/data/upload/mobile/special/s0/s0_05839307525338060.jpg" alt=""/>
+              <a style={{display:'block',height:'125px'}} href="http://www.51mkf.com/wap/tmpl/product_detail.html?goods_id=1232">
+                <img style={{display:'block',height:'125px'}} className='layout-f' src="http://www.51mkf.com/data/upload/mobile/special/s0/s0_05839425773470935.png" alt=""/>
+              </a>
+              <Layouti layouti={layouti1}></Layouti>
+            </li>
+            <li>
+              <img style={{display:'block',height:'43px'}} className='layout-f' src="http://www.51mkf.com/data/upload/mobile/special/s0/s0_05506778923821440.png" alt=""/>
+              <a style={{display:'block',height:'125px'}} href="http://www.51mkf.com/shop/cate-1367-0-0-0-0-0-0-0-0-0-0.html">
+                <img style={{display:'block',height:'125px'}} className='layout-f' src="http://www.51mkf.com/data/upload/mobile/special/s0/s0_05839476044017318.png" alt=""/>
+              </a>
+              <Layoutg layoutg={layouti2}></Layoutg>
+              <Layouti layouti={layouti3}></Layouti>
             </li>
 
           </ul>
