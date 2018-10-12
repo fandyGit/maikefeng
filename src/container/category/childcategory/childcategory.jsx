e20fe08122733622780e0e519f7c349f64b3ed01
luhuan917 <785470041@qq.com>
2018-10-12 14:08:36 +0800

    获取分类页的里层数据

--- src/container/category/childcategory/childcategory.jsx
+++ src/container/category/childcategory/childcategory.jsx
@@ -3,19 +3,31 @@ import {withRouter} from 'react-router-dom'
 import {reqCategory} from '../../../api/index'
 
 class Childcategory extends Component{
+  state={
+    imgs:{},
+    class_list:[],
+  }
   componentWillMount(){
     const id=this.props.match.params.id;
-    reqCategory(id).then(result=>{
-      console.log(result)
+    reqCategory(id).then(res=>{
+     this.setState({
+       imgs:res.datas['img'],
+       class_list:res.datas['class_list']
+     })
     });
   }
   componentWillReceiveProps(nextProps){
     const id=nextProps.match.params.id;
     reqCategory(id).then(res=>{
-      console.log(res)
+      this.setState({
+        imgs:res.datas.img,
+        class_list:res.datas['class_list']
+      })
     });
   }
   render(){
+    // console.log(this.state['class_list'])
+    // console.log(this.state.imgs)
     return (
       <div>
        {/*根据传递过来的id的值，请求获取数据渲染页面*/}
