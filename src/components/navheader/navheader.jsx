af298bda21c8fff3cd382f03a7b31e53ad5cf492
luhuan917 <785470041@qq.com>
2018-10-14 16:00:44 +0800

    修改各个页面小的问题

--- src/components/navheader/navheader.jsx
+++ src/components/navheader/navheader.jsx
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
 import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 import { NavBar, Icon ,InputItem,SearchBar} from 'antd-mobile';
 
@@ -9,13 +10,14 @@ class NavHeader extends Component{
     this.props.history.replace('/login');
   }
   render(){
+    const {username}=this.props.user;
     return (
       <div>
         <NavBar
-          mode="dark"
+          mode="light"
           icon={<i className='iconfont icon-saoma'></i>}
           onLeftClick={() => console.log('onLeftClick')}
-          rightContent={<i className='iconfont icon-denglu' onClick={this.handleLogin}></i>}
+          rightContent={<span style={{textAlign:'center',paddingLeft:'5px'}}>{username?username:<i className='iconfont icon-denglu' onClick={this.handleLogin}></i>}</span>}
         >
           <SearchBar placeholder='客官,查找你的商品'/>
         </NavBar>
@@ -23,4 +25,7 @@ class NavHeader extends Component{
     )
   }
 }
-export default withRouter(NavHeader)
+NavHeader=withRouter(NavHeader)
+export default connect(
+  state=>({user:state})
+)(NavHeader)
--- src/components/nctouch-nav-home/nctouch-nav-home.jsx
+++ src/components/nctouch-nav-home/nctouch-nav-home.jsx
@@ -1,35 +1,44 @@
 import React,{Component} from 'react'
 import { Flex, WhiteSpace} from 'antd-mobile';
+import {withRouter} from 'react-router-dom'
 
-export default class NctouchNavHome extends Component{
+class NctouchNavHome extends Component{
   state={
     imgs:[
       {
         id:1,
         title:'分类',
-        url:'http://www.51mkf.com/wap/images/browse_list_w.png'
+        url:'http://www.51mkf.com/wap/images/browse_list_w.png',
+        path:'/category'
       },
       {
         id:2,
         title:'购物车',
-        url:'http://www.51mkf.com/wap/images/cart_w.png'
+        url:'http://www.51mkf.com/wap/images/cart_w.png',
+        path:'/cart'
       },
       {
         id:3,
         title:'我的商城',
-        url:'http://www.51mkf.com/wap/images/member_w.png'
+        url:'http://www.51mkf.com/wap/images/member_w.png',
+        path:'/mine'
       },
       {
         id:4,
         title:'客户服务',
-        url:'http://www.51mkf.com/wap/images/mcc_04_w.png'
+        url:'http://www.51mkf.com/wap/images/mcc_04_w.png',
+        path:'/mine'
       },
       {
         id:5,
         title:'门店列表',
-        url:'http://www.51mkf.com/wap/images/service_w.png'
+        url:'http://www.51mkf.com/wap/images/service_w.png',
+        path:'/find'
       },
     ]
+  }
+  handleClick=(path)=>{
+    this.props.history.replace(path);
   }
   render(){
     return (
@@ -39,7 +48,7 @@ export default class NctouchNavHome extends Component{
           {
             this.state.imgs.map((item,index)=>{
               return (
-                  <Flex.Item key={index}>
+                  <Flex.Item key={index} onClick={()=>this.handleClick(item.path)}>
                     <img className='pic' src={item.url} alt={item.title}/>
                     <i>{item.title}</i>
                   </Flex.Item>
@@ -51,3 +60,4 @@ export default class NctouchNavHome extends Component{
     )
   }
 }
+export default withRouter(NctouchNavHome);
